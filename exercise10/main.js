587e91c6161c82a1aa342114601d060dac853959
ismaaciilmire <ismaaciilmire2@gmail.com>
2026-09-14 16:31:00 +0300

    exercise 9,10

---
+++ exercise10/main.js
@@ -0,0 +1,20 @@
+
+// array of object
+
+let library = [
+    {
+        title : "if u read more u khown more",
+        author :"ismail mohamed",
+        year  : 2022,
+    
+    },
+    {
+        title : "u dont think better then u",
+        author: "hanad jamac farax",
+        year: 2025,
+
+    }
+]
+console.log(library[0].title)
+console.log(library[1].author)
+
---
+++ exercise9/main.js
@@ -0,0 +1,11 @@
+let car = {
+    make : "hyunda",
+    model : 2010,
+    year : 2026,
+}
+
+console.log(car.make)
+console.log(car.model)
+console.log(car.year)
+car.start="the has start right now"
+console.log(car.start)
