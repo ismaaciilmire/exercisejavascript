6ee14a760251688be9cf3168f43c553c049a3e93
ismaaciilmire <ismaaciilmire2@gmail.com>
2026-09-16 16:17:07 +0300

    exer16,17

---
+++ exercise16/main.js
@@ -0,0 +1,9 @@
+let grade=70;
+
+if(grade >= 50){
+    console.log("you are passed")
+
+}
+else{
+    console.log("you are failed")
+}
---
+++ exercise17/main.js
@@ -0,0 +1,18 @@
+let temprature = 14;
+
+if(temprature < 0){
+    console.log("very cold")
+
+}
+else if(temprature < 15){
+    console.log("temprature is cold")
+
+}
+else if(temprature < 25 ){
+    console.log("temprature is warm")
+
+}
+else if (temprature > 25){
+    console.log("temprature is hot")
+
+}
